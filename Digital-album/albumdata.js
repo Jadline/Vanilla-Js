cecf24353392bb4d394db2942bbafa0caa3a71ff
Jadline <124354830+Jadline@users.noreply.github.com>
2024-08-09 13:55:28 +0000

    update changes

---
+++ Digital-album/albumdata.js
@@ -0,0 +1,41 @@
+const albumData = [
+    {
+        src : "Image-collections/floods.jpg",
+        text : "This photo captures a significant moment during the recent flood at our school"
+
+    },
+    {
+        src : "Image-collections/jad.jpg",
+        text : "A memorable shot of me at delmonte amani estate exploring the fields and capturing aerial footage of the beautiful landscape"
+
+    },
+    {
+        src : "Image-collections/floods.jpg",
+        text : "We went there to unwind and enjoy some quality time together, surrounded by the serene beauty of the place With my friend at mystical waters ruiru"
+
+    },
+    {
+        src : "Image-collections/research.jpg",
+        text :"I took this photo outside R & D delmonte kenya.Kind of made me really happy "
+    },
+    {
+        src : "Image-collections/study1.jpg",
+        text :"Just me and my books.I love school"
+    },
+    {
+        src : "Image-collections/sunrise1.jpg",
+        text :"The sunrise is a reflection of how creative God is"
+    },
+    {
+        src : "Image-collections/sunset.jpg",
+        text :"This particular photo normally excites me and just make me feel so connected to nature and i feel like its a beautiful thing"
+    },
+    {
+        src : "Image-collections/timetable.jpg",
+        text :"I love my study area.it just makes me feel motivated to wake up everyday and work harder"
+    },
+    {
+        src : "Image-collections/thikaroad.jpg",
+        text :"Just a random sunrise photo at my bus station as i was waiting to go to work.Its beautiful huh!"
+    }
+]
