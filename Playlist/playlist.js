0b1052c3179ba5366cf0204bef624743162aa130
Jadline <124354830+Jadline@users.noreply.github.com>
2024-08-15 09:37:24 +0000

    update changes

--- Playlist/playlist.js
+++ Playlist/playlist.js
@@ -1,66 +1,20 @@
-const playlistData = [
-    {
-        id : "1",
-        title : "That part",
-        src : "https://files.ceenaija.com/wp-content/uploads/music/2023/05/Lauren_Spencer-Smith_-_That_Part_CeeNaija.com_.mp3",
-        imagesrc : "https://i.scdn.co/image/ab67616d0000b273270a1c7644ec5a23c7d05272",
-        artist : "Lauren Spencer Smith",
-        duration : "2:55"
-    },
-    {
-        id : "2",
-        title : "Bigger Person",
-        src : "https://files.ceenaija.com/wp-content/uploads/music/2023/07/Lauren_Spencer-Smith_-_Bigger_Person_CeeNaija.com_.mp3",
-        imagesrc : "https://lastfm.freetls.fastly.net/i/u/ar0/4e389e223fbeaad221e4eeb0300c4c4e.jpg",
-        artist : "Lauren Spencer Smith",
-        duration : "2:41"
+import {playlistData} from "./playlistdata.js"
 
-    },
-    {
-        id : "3",
-        title : "Finger Crossed",
-        src : "https://files.ceenaija.com/wp-content/uploads/music/2022/05/Lauren_Spencer-Smith_-_Fingers_Crossed_CeeNaija.com_.mp3",
-        imagesrc : "https://cdnaws.recis.io/i/img/01/2c/d2/6c_9b6577_w320.jpg",
-        artist : "Lauren Spencer Smith",
-        duration : "3:07"
+let songsHTML = ""
+playlistData.forEach((song) => {
+    const artistparts = song.artist.split(" ")
+    let formattedArtist = artistparts.length === 3 ? `${artistparts[0]}  ${artistparts[1]} <br>${artistparts[2]} ` : song.artist
 
+    songsHTML += `
+        <div class="song-content">
+                <button class="song-data"><span class="song-title">${song.title}</span> <span class="song-artist">${formattedArtist}</span> <span class="song-duration">${song.duration}</span></button>
+        </div>
+    `
+    
+})
+document.querySelector('.song-container').innerHTML = songsHTML;
 
-    },
-    {
-        id : "4",
-        title : "Flowers",
-        src : "https://files.ceenaija.com/wp-content/uploads/music/2023/04/Lauren_Spencer-Smith_-_Flowers_CeeNaija.com_.mp3",
-        imagesrc : "https://source.boomplaymusic.com/group10/M00/07/19/5e75a55df8c54aa4b61d3bc1a7474a9c_464_464.jpg",
-        artist : "Lauren Spencer Smith",
-        duration : "2:45"
-    },
-    {
-        id : "5",
-        title : "28",
-        src : "https://files.ceenaija.com/uploads/music/2023/11/Ruth-B-feat-Dean-Lewis-28-(CeeNaija.com).mp3",
-        imagesrc : "https://source.boomplaymusic.com/group10/M00/03/14/6ab43d001abd4da394ada03bb110e35a_320_320.jpg",
-        artist : "Ruth B",
-        duration : "3:32"
-
-    },
-    {
-        id : "6",
-        title : "What about us",
-        src : "https://files.ceenaija.com/wp-content/uploads/music/2022/04/One_Voice_Children_s_Choir_-_What_About_Us_CeeNaija.com_.mp3",
-        imagesrc : "https://upload.wikimedia.org/wikipedia/en/6/65/Pink_What_About_Us.jpg",
-        artist : "Pink",
-        duration : "4:29"
-
-    },
-
-    {
-        id : "7",
-        title : "Rescue",
-        src : "https://files.ceenaija.com/wp-content/uploads/2019/07/Lauren-Daigle-Rescue-CeeNaija.com_.mp3",
-        imagesrc : "https://upload.wikimedia.org/wikipedia/en/c/c4/Rescue_%28Official_Single_Cover%29_by_Lauren_Daigle.png",
-        artist : "Lauren Daigle",
-        duration : "3:36" 
-
-    },
-
-]
+const song = document.querySelector('.song-data')
+song.addEventListener('click',() => {
+    console.log(song)
+})
